351977a63a24cfac42007fd883b022f9e8e40f65
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-07-30 15:05:46 +0200

    main steps

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-// generare una pssword con l'utente
+// generare una password con l'utente
 
-console.log('JS OK')
+/*
+1. Recupero l'elemnto dal DOM
+2. Chiedo all'utente il nome
+3. Chiedo all'utente il cognome
+4. chiedo all'utente il suo colore preferito
+5. Fase di elabaorazione
+6. inserisco il risultato nel DOM
+ */
